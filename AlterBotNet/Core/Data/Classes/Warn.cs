3584d52553452555912ef27ce7547c0303b63a76
Loïck Noa OBIANG NDONG <theekinnox@gmail.com>
2019-07-16 09:46:16 -0400

    -Début des commandes wiki et warn

---
+++ AlterBotNet/Core/Data/Classes/Warn.cs
@@ -0,0 +1,38 @@
+﻿#region USING
+
+using Discord;
+using Discord.WebSocket;
+
+#endregion
+
+namespace AlterBotNet.Core.Data.Classes
+{
+	public class Warn
+	{
+		#region PROPRIÉTÉS ET INDEXEURS
+
+		public ulong WarnedUser { get; }
+		private string Reason { get; }
+
+		#endregion
+
+		#region CONSTRUCTEURS
+
+		public Warn(ulong pWarnedUser, string pReason)
+		{
+			this.WarnedUser = pWarnedUser;
+			this.Reason = pReason;
+		}
+
+		#endregion
+
+		#region MÉTHODES
+
+		public override string ToString()
+		{
+			return $"{this.WarnedUser.ToString()};{this.Reason}";
+		}
+
+		#endregion
+	}
+}
